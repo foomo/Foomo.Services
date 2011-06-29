82aec4e2f988e51e754781281af3d64be4ffd8bd
franklin <franklin@weareinteractive.com>
2011-06-29 19:51:53 +0200

    added license note

--- lib/Foomo/Services/ProxyGenerator/ActionScript/AbstractGenerator.php
+++ lib/Foomo/Services/ProxyGenerator/ActionScript/AbstractGenerator.php
@@ -1,5 +1,22 @@
 <?php
 
+/*
+ * This file is part of the foomo Opensource Framework.
+ *
+ * The foomo Opensource Framework is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License as
+ * published  by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The foomo Opensource Framework is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * the foomo Opensource Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace Foomo\Services\ProxyGenerator\ActionScript;
 
 use Foomo\Flash\ActionScript\PHPUtils;
@@ -28,7 +45,7 @@ abstract class AbstractGenerator extends \Foomo\Services\Renderer\AbstractRender
 	 */
 	protected $classFiles;
 	/**
-	 * class files namely ASClasses, which need to go in extra packages aka the sharedVo√î√∏Œ©s
+	 * class files namely ASClasses, which need to go in extra packages aka the sharedVo‚àö√Æ‚àö‚àè≈í¬©s
 	 *
 	 * hash filename => filecontents
 	 *
